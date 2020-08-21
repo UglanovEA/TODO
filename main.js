df6d678d2771b02f97b1cd41fccd2fafc31bc7d8
UglanovEA <uglanovea@gmail.com>
2020-08-21 17:56:48 +0300

    ver 1.1

---
+++ main.js
@@ -0,0 +1,5 @@
+const selectAllButton = document.getElementById("test");
+const inputElement = document.getElementById("input");
+
+selectAllButton.addEventListener("click", () => {});
+inputElement.addEventListener("keydown", () => {});
