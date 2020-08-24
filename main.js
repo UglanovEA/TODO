0d91cde191bc910ef3ea52c250b51a14a87a9f3e
UglanovEA <uglanovea@gmail.com>
2020-08-24 20:10:01 +0300

    ver 1.2

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const selectAllButton = document.getElementById("test");
 const inputElement = document.getElementById("input");
+const todoList = [];
 
-selectAllButton.addEventListener("click", () => {});
-inputElement.addEventListener("keydown", () => {});
+inputElement.addEventListener("keydown", (event) => {
+  if (event.key === "Enter" || event.keyCode === 13) {
+    todoList.push(inputElement.value);
+    inputElement.value = "";
+  }
+});
