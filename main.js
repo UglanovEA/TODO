112fe98f609309396fca7020895f0649a2f0508a
UglanovEA <uglanovea@gmail.com>
2022-04-27 11:31:28 +0300

    ver 1.5

--- main.js
+++ main.js
@@ -1,117 +1,90 @@
-const inputElement = document.getElementById("input");
-const listElement = document.getElementById("list");
-let todoList = [];
+var todoWrapper = document.querySelector('.todo__wrapper');
+var input = document.querySelector('.todo__input');
+var addBtn = document.querySelector('.icon-add');
+var deleteAllBtn = document.querySelector('.icon-delete');
+var completedBtn = document.querySelector('.icon-completed');
+var removeBtn = document.querySelector('.icon-trash');
 
-inputElement.addEventListener("keydown", (event) => {
-  if (event.key === "Enter" || event.keyCode === 13) {
-    todoList.unshift({
-      content: inputElement.value, //обьект со св-вом content
-      selected: false, //выбран ли данный элемент или нет
-      done: false, //св-во done яв-ся ли оно выполненым
-    }); // после нажатия enter добавляем в массив объект
-    inputElement.value = ""; //после добавления очищаем input
-    updateView();
+addBtn.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  /* Create elements */
+  if (input.value.trim()) {
+    /* UL */
+    var ulTag = document.createElement('ul');
+    ulTag.classList.add('todo-list__items');
+
+    /* LI */
+    var liTag = document.createElement('li');
+    liTag.innerText = input.value;
+    liTag.classList.add('todo-list__item');
+
+    /* Button */
+    var buttonDiv = document.createElement('div');
+    buttonDiv.classList.add('todo-list__button');
+
+    /* Completed button  */
+    var completedBtn = document.createElement('button');
+    completedBtn.classList.add('icon-completed');
+    completedBtn.innerHTML = '<i class="ri-check-line"></i>';
+
+    /* Edit button */
+    var editBtn = document.createElement('button');
+    editBtn.classList.add('icon-edit');
+    editBtn.innerHTML = '<i class="ri-edit-2-fill"></i>';
+    editBtn.onclick = function () {
+      editWorking(liTag);
+    };
+
+    /* Trash button */
+    var trashBtn = document.createElement('button');
+    trashBtn.classList.add('icon-trash');
+    trashBtn.innerHTML = '<i class="ri-delete-bin-line"></i>';
+
+    /* Appending Elements into each other */
+    ulTag.appendChild(liTag);
+    liTag.appendChild(buttonDiv);
+    buttonDiv.appendChild(completedBtn);
+    buttonDiv.appendChild(editBtn);
+    buttonDiv.appendChild(trashBtn);
+
+    /* if input is empty then don't display empty list in DOM */
+    todoWrapper.appendChild(ulTag);
+
+    /* sessionStorage */
+    /* when the add button click clear the input value */
+    input.value = '';
+    /* completed and trash button working */
+    liTag.addEventListener('click', function (e) {
+      var items = e.target;
+      if (items.classList[0] === 'icon-completed') {
+        var todo = items.parentElement;
+        var todo2 = todo.parentElement;
+        todo2.classList.add('line-through');
+      } else if (items.classList[0] === 'icon-trash') {
+        var todo = items.parentElement;
+        var todo2 = todo.parentElement;
+        todo2.classList.add('fall');
+        todo2.addEventListener('transitionend', function () {
+          var todo3 = todo2.parentElement;
+          todo3.remove();
+        });
+      }
+    });
+  } else if (input.value === '') {
+    alert('please fill the input field');
   }
 });
 
-function updateView() {
-  listElement.innerHTML = ""; //чистка внутри ul
-
-  for (let index = 0; index < todoList.length; index++) {
-    //обошли массив, теперь есть переменная index которая хранит порядковый номер
-    //перебираем с учетом порядка
-    const todoItem = todoList[index];
-
-    const liElement = document.createElement("li"); // создали li элемент
-    liElement.className = "list-group-item"; // назначили класс
-    listElement.append(liElement); //добавим в list элемент
-
-    const divElement = document.createElement("div"); //создали div элемент
-    divElement.className = "form-group form-check"; //назначили класс
-    liElement.append(divElement); //добавим div в li элемент
-
-    const checkboxElement = document.createElement("input"); //создали checkbox элемент
-    divElement.append(checkboxElement); //добавили элемент
-    checkboxElement.type = "checkbox"; //назначили тип
-    checkboxElement.className = "form-check-input"; //назначили класс
-    checkboxElement.checked = todoItem.selected; //если todoItem выбран, то будет включен checked
-    checkboxElement.id = "todoItem" + index;
-
-    const labelElement = document.createElement("label"); //создали label элемент
-    divElement.append(labelElement); // добавили элемент
-    labelElement.className = "form-check-label"; //назначили класс
-    if (todoItem.done) {
-      //если todoitem выполнен, назначаем ему класс todoDone
-      labelElement.className += " todoDone";
-    }
-    labelElement.setAttribute("for", "todoItem" + index); //задали for
-    labelElement.innerText = todoItem.content; // подставляем контент которые пишем в input
-
-    const buttonDoneElement = document.createElement("button"); //создали button элемент
-    divElement.append(buttonDoneElement); //добавили элемент
-    buttonDoneElement.type = "button"; //назначили тип
-    buttonDoneElement.className = "btn btn-outline-primary"; //назначили класс
-    buttonDoneElement.innerText = "Done"; // дали название
-    buttonDoneElement.style = "float: right";
-
-    const buttonRemoveElement = document.createElement("button"); //создали button элемент
-    divElement.append(buttonRemoveElement); //добавили элемент
-    buttonRemoveElement.type = "button"; //назначили тип
-    buttonRemoveElement.className = "btn btn-outline-danger"; //назначили класс
-    buttonRemoveElement.innerText = "Remove"; // дали название
-    buttonRemoveElement.style = "float: right; margin-right: 10px;";
-
-    buttonDoneElement.addEventListener("click", () => {
-      todoItem.done = !todoItem.done; //меняем событие на противоположное
-      updateView(); //обновляем вид
-    });
-
-    checkboxElement.addEventListener("change", () => {
-      todoItem.selected = checkboxElement.checked;
-    });
-
-    buttonRemoveElement.addEventListener("click", () => {
-      todoList = todoList.filter(
-        //удаляем значения по кнопке remove
-        (currentTodoItem) => currentTodoItem !== todoItem
-      );
-      updateView();
-    });
-  }
+function editWorking(e) {
+  var editValue = prompt('edit the select item', e.firstChild.nodeValue);
+  e.firstChild.nodeValue = editValue;
 }
 
-document.getElementById("doneAction").addEventListener("click", () => {
-  for (const todoItem of todoList) {
-    //перебираем элементы массива
-    if (todoItem.selected) {
-      //если элемент массива выбран
-      todoItem.done = true; //то меняем статус на сделано - true
-      todoItem.selected = false; // убираем галочки после группового выбора
-    }
+function deleteAllElements() {
+  var gettingUlTag = document.querySelectorAll('.todo-list__items');
+  for (var i = 0; i < gettingUlTag.length; i++) {
+    gettingUlTag[i].remove();
   }
-  updateView();
-});
-
-document.getElementById("restoreAction").addEventListener("click", () => {
-  for (const todoItem of todoList) {
-    if (todoItem.selected) {
-      todoItem.done = false;
-      todoItem.selected = false;
-    }
-  }
-  updateView();
-});
-
-document.getElementById("removeAction").addEventListener("click", () => {
-  todoList = todoList.filter((todoItem) => !todoItem.selected); //делаем фильтрацию, выбранные элементы удаляем
-  updateView();
-});
-
-document.getElementById("selectAllAction").addEventListener("click", () => {
-  for (const todoItem of todoList) {
-    //кнопка проходит по всему циклу,  и делает все элементы выделеными
-    todoItem.selected = true;
-  }
-
-  updateView();
-});
+  input.value = '';
+}
